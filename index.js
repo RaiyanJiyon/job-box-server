8a1f4c84a5d1382aef5e8a61c0429d3359af6ca0
RaiyanJiyon <raiyanjiyon814@gmail.com>
2025-02-09 21:19:47 +0600

    Added jobs get http method by specific job id

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = 5000;
 app.use(cors()); // Enable CORS for all routes
 app.use(express.json()); // Parse JSON bodies
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.5qizv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -44,6 +44,23 @@ async function run() {
                 res.status(500).send({ message: 'Failed to fetch job data' });
             }
         });
+
+        app.get('/jobs/:id', async (req, res) => {
+            try {
+                const id = req.params.id;
+                const query = {_id: new ObjectId(id)};
+                const result = await jobsCollection.findOne(query);
+
+                if (result) {
+                    res.status(200).send(result);
+                } else {
+                    res.status(404).send({ message: 'Job data not found!' });
+                }
+            } catch (error) {
+                console.error('Error fetching job data:', error);
+                res.status(500).send({ message: 'Failed to fetch job data' });
+            }
+        })
 
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
